d4a772d85522b639bb0d37df9f6f6ae21d15ca93
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-09-04 13:28:46 +0200

    a

---
+++ job-02/index.php
@@ -0,0 +1,129 @@
+<?php
+
+// Class Product :
+class Product{
+    // Properties :
+    private int $id;
+    private string $name;
+    private array $photos;
+    private int $price;
+    private string $description;
+    private int $quantity;
+    private Datetime $createdAt;
+    private Datetime $updatedAt;
+
+
+    // Constructor to initiate properties :
+    public function __construct(int $id, string $name, array $photos, int $price, string $description, int $quantity, Datetime $createdAt, Datetime $updatedAt){
+        $this->id = $id;
+        $this->name = $name;
+        $this->photos = $photos;
+        $this->price = $price;
+        $this-> description = $description;
+        $this->quantity = $quantity;
+        $this->createdAt = $createdAt;
+        $this->updatedAt = $updatedAt;
+    } // Constructor closed
+
+
+    // Getters :
+    public function getId(): int{
+        return $this->id;
+    }
+
+    public function getName(): string{
+        return $this->name;
+    }
+
+    public function getPhotos(): array{
+        return $this->photos;
+    }
+
+    public function getPrice(): int{
+        return $this->price;
+    }
+
+    public function getDescription(): string{
+        return $this->description;
+    }
+
+    public function getQuantity(): int{
+        return $this->quantity;
+    }
+
+    public function getCreatedAt(): Datetime{
+        return $this->createdAt;
+    }
+
+    public function getUpdatedAt(): Datetime{
+        return $this->updatedAt;
+    }
+
+
+    // Setters : 
+    public function setId(int $id): void{
+        $this->id = $id;
+    }
+
+    public function setName(string $name): void{
+        $this->name = $name;
+    }
+
+    public function setPhotos(array $photos): void{
+        $this->photos = $photos;
+    }
+
+    public function setPrice(int $price): void{
+        $this->price = $price;
+    }
+
+    public function setDescription(string $description): void{
+        $this->description = $description;
+    }
+
+    public function setQuantity(int $quantity): void{
+        $this->quantity = $quantity;
+    }
+
+    public function setCreatedAt(DateTime $createdAt): void{
+        $this->createdAt = $createdAt;
+    }
+
+    public function setUpdatedAt(DateTime $updatedAt): void{
+        $this->updatedAt = $updatedAt;
+    }
+
+} // Class Product closed
+
+
+// Class Category :
+class Category{
+    private int $id;
+    private string $name;
+    private string $description;
+    private DateTime $createdAt;
+    private DateTime $updatedAt;
+}
+
+// Class Product test
+$product = new Product(1, "Laptop", ["photoLaptop.jpg"], 1500, "A high-performance laptop", 10, new DateTime('now'), new DateTime('now'));
+
+// Use of getters
+var_dump($product->getId());
+var_dump($product->getName());
+var_dump($product->getPhotos());
+var_dump($product->getPrice());
+var_dump($product->getDescription());
+var_dump($product->getQuantity());
+var_dump($product->getCreatedAt());
+var_dump($product->getUpdatedAt());
+
+// Use of setters
+$product->setPrice(1200);
+$product->setQuantity(9);
+
+// Modifications checking :
+var_dump($product->getPrice());
+var_dump($product->getQuantity())
+
+?>
